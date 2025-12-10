fd43c0330e5dc0089e999f515d64ae7e22799a88
ZahidHasan000 <h.zahid007.zh59@gmail.com>
2025-12-11 02:53:09 +0600

    11dec

--- server.js
+++ server.js
@@ -21,10 +21,12 @@ const limiter = rateLimit({
 dotenv.config();
 const app = express();
 
-app.use(cors({
-  origin: 'https://portfolio-frontend-lurr.onrender.com' // Allow requests only from this origin
-  // origin: 'http://localhost:5173' // Allow requests only from this origin
-}));
+// app.use(cors({
+//   origin: 'https://portfolio-frontend-lurr.onrender.com' // Allow requests only from this origin
+//   // origin: 'http://localhost:5173' // Allow requests only from this origin
+// }));
+
+app.use(cors());
 
 
 // app.use(express.json({ limit: '10kb' }));
