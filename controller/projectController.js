2e978dabfe191a55ef034010e4d013b896ea274b
ZahidHasan000 <h.zahid007.zh59@gmail.com>
2025-12-12 01:15:39 +0600

    12dec

--- controller/projectController.js
+++ controller/projectController.js
@@ -4,13 +4,29 @@ const path = require("path");
 const multer = require('multer');
 const fs = require('fs');
 
+// const uploadDir = path.join(__dirname, '../public');
+
+// const storage = multer.diskStorage({
+//   destination(req, file, cb) {
+//     if (!fs.existsSync(uploadDir)) {
+//       fs.mkdirSync(uploadDir, { recursive: true });
+//     }
+//     cb(null, uploadDir);
+//   },
+//   filename(req, file, cb) {
+//     cb(null, Date.now() + '_' + file.originalname);
+//   }
+// });
+
 const uploadDir = path.join(__dirname, '../public');
+
+// Make sure the directory exists
+if (!fs.existsSync(uploadDir)) {
+  fs.mkdirSync(uploadDir, { recursive: true });
+}
 
 const storage = multer.diskStorage({
   destination(req, file, cb) {
-    if (!fs.existsSync(uploadDir)) {
-      fs.mkdirSync(uploadDir, { recursive: true });
-    }
     cb(null, uploadDir);
   },
   filename(req, file, cb) {
